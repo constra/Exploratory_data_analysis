7607494db7a76212749fabf624b2a6bdf4ebbe4e
Sun Mai <sun.mai@outlook.com>
2015-02-15 09:14:19 +0100

    ggplot

--- ggplot_example.R
+++ ggplot_example.R
@@ -9,3 +9,20 @@ qplot(displ,hwy,data = mpg)
 with(mpg,plot(displ,hwy))
 # color
 qplot(displ,hwy,data = mpg,color=drv)
+
+# add geometry
+
+qplot(displ,hwy,data = mpg,geom=c("point","smooth"))
+
+# histogram
+qplot(hwy,data=mpg,fill=drv)
+
+# panels
+qplot(displ,hwy,data = mpg, facets = .~ drv)
+
+qplot(displ,hwy,data = mpg, facets = drv ~.)
+## histogram in panels
+qplot(hwy,data=mpg,facets = drv ~.,binwidth=2)
+
+
+str(maacs)
