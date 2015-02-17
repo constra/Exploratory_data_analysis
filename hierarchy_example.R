3241d31ead83f4ce6063426b3879d477834bba33
Sun Mai <sun.mai@outlook.com>
2015-02-17 19:57:42 +0100

    add PCA/SVD

--- hierarchy_example.R
+++ hierarchy_example.R
@@ -44,3 +44,30 @@ kmeansObj2 <- kmeans(dataMatrix,centers = 3)
 par(mfrow = c(1,2),mar = c(2,4,0.1,0.1))
 image(t(dataMatrix)[,nrow(dataMatrix):1],yaxt = "n")
 image(t(dataMatrix)[, order(kmeansObj$cluster)],yaxt = "n")
+
+## PCA and SVD
+#matrix data
+set.seed(12345)
+par(mar = rep(0.2,4))
+dataMatrix <- matrix(rnorm(400),nrow = 40)
+image(1:10,1:40,t(dataMatrix)[,nrow(dataMatrix):1])
+par(mar= rep(0.2,4))
+heatmap(dataMatrix)
+
+# add some patter
+set.seed(678910)
+for (i in 1:40){
+        coinFlip <- rbinom(1,size=1,prob=0.5)
+        if (coinFlip){
+                dataMatrix[i,] <- dataMatrix[i,] + rep(c(0,3), each = 5)
+        }
+}
+image(1:10,1:40,t(dataMatrix)[,nrow(dataMatrix):1])
+heatmap(dataMatrix)
+
+hh <- hclust(dist(dataMatrix))
+dataMatrixOrdered <- dataMatrix[hh$order, ]
+par(mfrow = c(1, 3))
+image(t(dataMatrixOrdered)[, nrow(dataMatrixOrdered):1]) 
+plot(rowMeans(dataMatrixOrdered), 40:1, , xlab = "Row Mean", ylab = "Row", pch = 19) 
+plot(colMeans(dataMatrixOrdered), xlab = "Column", ylab = "Column Mean", pch = 19)
